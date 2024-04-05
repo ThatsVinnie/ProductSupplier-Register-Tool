4ba2f331722f9db4d4485d6105a2b1206660aed4
Vinícius Barbosa <vinibar95@gmail.com>
2024-04-05 19:09:43 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain
+{
+    // Declaração da entidade Product
+    public class Product
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public int IdSupplier {  get; set; }
+    }
+}
